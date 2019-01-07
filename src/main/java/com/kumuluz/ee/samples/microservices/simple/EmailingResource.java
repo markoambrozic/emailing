dde7c5adfd4ced1ebcf67e1b09a57b0ae1d2603d
Lamp <dmvoicestudios@gmail.com>
2019-01-07 20:48:44 +0100

    Added mail function

--- src/main/java/com/kumuluz/ee/samples/microservices/simple/EmailingResource.java
+++ src/main/java/com/kumuluz/ee/samples/microservices/simple/EmailingResource.java
@@ -41,7 +41,6 @@ public class EmailingResource {
         JSONObject obj = new JSONObject();
         //send mail here from url params
 
-        //TODO url params
         //TODO da je pro SMTP server
 
         obj.put("emailing", "fancy info about people");
@@ -101,4 +100,47 @@ public class EmailingResource {
         return Response.status(Response.Status.OK).entity(emailJSON).build();
     }
 
+    public void sendFromGMail(String from, String pass, String[] to, String subject, String body) {
+        Properties props = System.getProperties();
+        String host = "smtp.gmail.com";
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.user", from);
+        props.put("mail.smtp.password", pass);
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+
+        Session session = Session.getDefaultInstance(props);
+        MimeMessage message = new MimeMessage(session);
+
+        try {
+            message.setFrom(new InternetAddress(from));
+            InternetAddress[] toAddress = new InternetAddress[to.length];
+
+            // To get the array of addresses
+            for( int i = 0; i < to.length; i++ ) {
+                toAddress[i] = new InternetAddress(to[i]);
+            }
+
+            for( int i = 0; i < toAddress.length; i++) {
+                message.addRecipient(Message.RecipientType.TO, toAddress[i]);
+            }
+
+            message.setSubject(subject);
+            message.setText(body);
+            Transport transport = session.getTransport("smtp");
+            transport.connect(host, from, pass);
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+        }
+        catch (AddressException ae) {
+            ae.printStackTrace();
+        }
+        catch (MessagingException me) {
+            me.printStackTrace();
+        }
+    }
+
 }
+
+
